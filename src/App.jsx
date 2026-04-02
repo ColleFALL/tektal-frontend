14a3b0229288107c0923eb104eec5747fa1809ef
ColleFALL <collefall118@gmail.com>
2026-04-02 23:35:58 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import './App.css'
+// import './index.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold bg-red-500 text-white px-6 py-3 rounded-lg">
+        Hello Tektal 
+      </h1>
+    </div>
+  )
+}
+
+export default App
+  
+  
+
+
